0d8eb05a76034c0262050f555efe33c352a78a2d
ningjngjing0614 <215937309@qq.com>
2019-11-16 14:37:38 +0800

    代码练习

---
+++ 2019-11-08/2019-11-08/2019-11-08.cpp
@@ -0,0 +1,36 @@
+#include<stdio.h>
+//int main()
+//{
+//	int a=3;
+//	int b=3;
+//printf("%d %d %d",a++,++b,a);
+//	int i=0,a=1,b=2,c=3,d=4;
+//
+//	printf("%d%d%d%d",a++,++b,c,d++);
+//return 0;
+//}
+//struct stu 
+//{
+//char name[20];
+//short age;
+//char id[20];
+//};
+//
+//int main()
+//{
+//	struct stu a = {"ÕÅÈı",20,"2020200"};
+//	struct stu* ta=NULL;
+//	printf("%s %d %s\n", a.name , a.age , a.id );
+//	ta = &a;
+//	printf("%s\n",(* ta).name);
+//	printf("%s\n",ta->id);
+//	return 0;
+//}
+int main()
+{
+	char a = 3;
+	char b = 94;
+	char c = a+b;
+	printf ("%c\n",c);
+	return 0;
+}
