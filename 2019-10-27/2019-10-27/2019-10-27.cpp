605c2980cce6b1c24f59d88e6c758429e339073a
ningjngjing0614 <215937309@qq.com>
2019-11-17 14:22:27 +0800

    代码练习

---
+++ 2019-10-27/2019-10-27/2019-10-27.cpp
@@ -0,0 +1,55 @@
+
+	//打印出100~200之间的素数
+//#include <stdio.h>
+//int main()
+//{
+//	int i,j,k;
+//	for (i=100;i<=200;i++)
+//	{
+//		int k=0;
+//		for (j=2;j<i;j++)
+//		{
+//			if (i%j==0)
+//				k=1;
+//		}
+//		if(k==0)
+//			printf("%d\n",i);
+//	}
+//	return 0;
+//}
+//打印乘法口诀表
+//#include <stdio.h>
+//int main()
+//{
+//	int i,j,k;
+//	{
+//		for (i=1;i<10;i++)
+//		{
+//			for (j=1;j<=i;j++)
+//			{
+//				k=i*j;
+//			printf ("%dX%d=%d\t",j,i,k);
+//			}
+//			printf("\n");
+//		}
+//	}
+//	return 0;
+//}
+//判断1000年-2000年之间的闰年
+//#include <stdio.h>
+//int main()
+//{
+//	int year;
+//	{
+//		for(year=1000;year<=2000;year++)
+//		{
+//			if(year%4==0&&year%100!=0)
+//				printf("%d为闰年\n",year);
+//			if(year%400==0)
+//				printf("%d为闰年\年",year);
+//			else 
+//				printf("%d不是闰年\n",year);
+//		}
+//	}
+//	return 0;
+//}
