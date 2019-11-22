75c2c06878819e32b104faacef8fcc02c1fef6b7
ningjngjing0614 <215937309@qq.com>
2019-11-22 16:19:05 +0800

    练习

---
+++ Fhomework.java
@@ -0,0 +1,88 @@
+// import java.util.Scanner;
+// public class Fhomework {
+//     public static void main(String[] args) {
+        // byte a = (byte)130;
+        // System.out.println("a is :"+a);
+        // byte a = 10;
+        // byte  b =20;
+        // byte c = (byte)(a+b);
+        // final byte a = 20;
+        // final byte b = 20;
+        // byte c = (byte)(a+b);
+        // char a = 'a';
+        // int b = a-32;
+        // char c = (char)b;
+        // String str = "123";
+        // int c = Integer.parseInt(str);
+        // int day = 1;
+        // switch( day){
+        //     case 1:
+        //         System.out.println(11111);
+        //     case 2:
+        //         System.out.println(22222);
+        // }
+        // Scanner sc = new Scanner(System.in);
+        // int a = sc.nextInt();
+        // double b = sc.nextDouble();
+        // String name =null;
+        // if (sc.hasNextLine()){
+        //  name = sc.next();
+        // }
+        // System.out.println("int is"+ a+";double is"+ b +";string is"+ name);
+        // int sum = 0;
+        // for (int i = 1; i < 6; i++){
+        //     int temp = 1;
+        //     for (int j = 1; j<=i; j++){
+        //         temp *=j;
+        //     }
+        //     sum +=temp;
+        // System.out.println("sum is "+sum);
+        // }
+        //  Scanner t = new Scanner (System.in);
+        //  int a = t. nextInt(); 
+        //  System.out.println("sum is "+a);
+        
+
+    //     int a = 10;
+    //     int b = 20;
+    //     int c = sum (a,b);
+    //     System.out.println("sum is "+c);
+
+    // }
+    // public static int sum(int a,int b){
+    //     int s = a+b;
+    //     return s;
+    // }
+//        test();
+//     }
+//    public static void test(){
+//        System.out.println("sum is ");
+  //  }
+  import java.util.Random;
+  public class Fhomework{
+      public static void main(String[] args){
+    //    int a = 5;
+    //    int f = factor (a);
+    //    System.out.println("sum is "+f);
+    //   }
+    //   public static int factor(int n){
+    //        System.out.println("sum is111111 "+n);
+    //       if (n ==1){
+    //           System.out.println("sum is 2222222   "+n);
+    //           return 1;
+    //       }
+    //       int s = n*factor(n-1);
+    //       System.out.println("sum is 33333"+n);
+    //       return s;
+            // System.out.println("sum is" +s);
+    // int [] a =new int [] {1,2,3,4,5};
+    // System.out.println("sum is "+a); 
+    // System.out.println("sum is "+b);
+    // for (int i = 0; i< b.length; i++){
+    //     System.out.println(b[i]);
+    //}
+       Random random = new Random();
+       int a = random.nextInt(50);
+       System.out.println("sum is "+a);
+    }
+  }
