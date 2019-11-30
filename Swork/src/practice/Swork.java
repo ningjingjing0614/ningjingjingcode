2b2e11ae9ed11c98dc0f6057b54a66838581afca
ningjngjing0614 <215937309@qq.com>
2019-11-30 13:20:33 +0800

    练习

---
+++ Swork/src/practice/Swork.java
@@ -0,0 +1,39 @@
+package practice;
+//class Person {
+//    public String name ;
+//    public int age ;
+//    public String sex ;
+//    public Person(){
+//        this("ningjingjing",19,"girl");
+//    }
+//    public Person(String name,int age,String sex){
+//        this.name = name;
+//        this.age = age;
+//        this.sex = sex;
+//    }
+//    public void show(){
+//        System.out.println(name + age + sex);
+//    }
+//}
+//public class Swork {
+//    public static void main(String[] args) {
+//        Person per1 = new Person();
+//        per1.show();
+//
+//    }
+//}
+//class ThisPerson{
+//    public void test(){
+//        System.out.println("test中的"+this);
+//    }
+//}
+//public class Swork{
+//    public static void main(String[] args) {
+//        ThisPerson per1 = new ThisPerson();
+//        System.out.println("main中的"+per1);
+//        per1.test();
+//        ThisPerson per2 = new ThisPerson();
+//        System.out.println("main中的"+per2);
+//        per2.test();
+//    }
+//}
