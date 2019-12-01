908e274dd64cfab2f5e685cbf126007762434db4
ningjngjing0614 <215937309@qq.com>
2019-12-01 21:46:55 +0800

    练习

---
+++ Twork/src/review/Twork.java
@@ -0,0 +1,47 @@
+package review;
+import java.util.Arrays;
+//import java.util.Scanner;
+//public class Twork {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("input two numbers:");
+//        int a = sc.nextInt();
+//        int b = sc.nextInt();
+//       int c = iadd(a,b);
+//        System.out.println(c);
+//        double d = dadd(45.4,78.6);
+//        System.out.println(d);
+//    }
+//    public static int iadd(int x,int y){
+//        return (x + y);
+//    }
+//    public static double dadd(double x,double y){
+//        return (x + y);
+//    }
+//}
+//public class Twork{
+//    public static void main(String[] args) {
+//        int num = 0;
+//        fun(num);
+//        System.out.println(num);
+//    }
+//
+//    public static void fun(int n) {
+//        n = 119;
+//        System.out.println("n="+n);
+//    }
+//}
+public class Twork{
+    public static void main(String[] args) {
+//        int[] arr = {4,7,8};
+//        fun(arr);
+//        System.out.println("arr[0]"+arr[0]);
+//    }
+//    public static void fun (int[] array){
+//        array[0] = 10;
+//        System.out.println("array[0] = "+array[0]);
+        int[] arr = {1,5,6,8,9,3};
+        String newarr = Arrays.toString(arr);
+        System.out.println(newarr);
+    }
+}
